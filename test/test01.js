92fbcdd40b2ffd0df6fa1854440d60ac7136ce81
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-06-12 23:14:21 +0200

    tests passed

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,17 @@
 function clasificaElementos(index) {
-    return [0,0,0];
+    let nPositivo = [];
+    let nNegativo = [];
+    let nCero = []
+    for(let i = 0; i < index.length; i++){
+        if(index[i] > 0){
+            nPositivo.push(index[i])
+        }else if(index[i] === 0){
+            nCero.push(index[i])
+        }else{
+            nNegativo.push(index[i])
+        }
+    }
+  let result = [(nPositivo.length/index.length).toFixed(4), (nNegativo.length/index.length).toFixed(4), (nCero.length/index.length).toFixed(4)]
+  return result
 }
 module.exports = clasificaElementos;
